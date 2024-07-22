5467f7b37df4b9e822fd05fb5ead7c661e38a451
clarkera <rclarke9941@gmail.com>
2024-07-22 17:05:05 -0400

    update

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/clarkera/myreactportfolio.git"
+      className="link footer__link"
+    >
+      Created By Rachael Clarke
+    </a>
+  </footer>
+);
+
+export default Footer;
